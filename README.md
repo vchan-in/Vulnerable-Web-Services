f2902540f6f044dc4a93df85d1f54fa86de13a7c
Mukul Kantiwal <mukkul007@gmail.com>
2018-12-12 13:36:07 +0530

    alpha

---
+++ README.md
@@ -0,0 +1,39 @@
+# Vulnerable Web Services by MK
+
+
+Web Services by MK is an insecure web application with multiple vulnerable web service components that can be used to learn real world web service vulnerabilities. The aim of this project is to help security professionals learn about Web Application Security through the use of a practical lab environment.
+
+
+
+This application includes the following vulnerabilities.
+
++ WSDL Enumeration
++ XML External Entity Injection
++ XML Bomb Denial-of-Service
++ XPATH Injection
++ WSDL Scanning
++ Cross Site-Tracing
++ OS Command Injection
++ Server Side Request Forgery
++ REST API SQL Injection
++ Same Origin Method Execution
++ JSON Web Token (JWT) Secret Key Brute Force
++ Cross-Origin Resource Sharing
+
+
+
+
+# Instructions
+Web Services can be used with a XAMPP setup. XAMPP is a free and open source cross-platform web server solution which mainly consists of an Apache Web Server and MySQL database. To setup, download and install the XAMPP setup first. 
+
+Note: PHP 5.5.38 is required for most of the exercises to work correctly.
+
+# Disclaimer
+Do not host this application on live or production environment.
+
+
+# To Do list
+
++ JSON Hijacking
++ SOAP Injection
++ XML Injection
---
+++ about/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Setup Instructions</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/dvws/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/dvws/css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+       <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="/dvws/index.html">
+                        Home
+                    </a>
+                </li>
+                <li><a href="/dvws/about/">About</a></li>
+                <li><a href="/dvws/about/instructions.php">Setup instructions</a></li>
+                <li><a href="/dvws/appinfo.php">PHP Information</a></li>
+        
+                                <li class="sidebar-brand">
+                    <a href="/dvws/vulnerabilities/">
+                        Vulnerabilities
+                    </a>
+                <li><a href="/dvws/vulnerabilities/wsdlenum/">WSDL Enumeration</a></li>
+                <li><a href="/dvws/vulnerabilities/xmlbomb/xmlbomb.php">XML Bomb Denial-of-Service</a></li>
+                <li><a href="/dvws/vulnerabilities/xxe/">XML External Entity Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/xpath/xpath.php">XPATH Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/cmdi/client.php">Command Injection</a></li>
+                <li><a href="/dvws/vulnerabilities/xst/xst.php">Cross Site Tracing (XST)</a></li>
+                <li><a href="/dvws/vulnerabilities/ssrf/">Server Side Request Forgery</a></li>
+                <li><a href="/dvws/vulnerabilities/sqli/">REST API SQL Injection</a></li>
+				   <li><a href="/dvws/vulnerabilities/xxe2/">XML External Entity Injection 2</a></li>
+                
+            </ul>
+            
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+ 
+ 
+ 
+ 
+ 
+                        <h1>About</h1>
+                        
+ <p>This application was created mainly due to the lack of practical environments available for hacking web services.  Vulnerable applications such as DVWA and Mutillidae focuses more on generic Web application vulnerabilities such as Cross-site Scripting and SQL Injection.</p>                             
+
+
+<!--    <li><a href="https://www.youtube.com/watch?v=oPrrFNEasgE">Greg Patton -The API Assessment Primer (AppSecEU) 2015</a></li>
+   <li><a href="https://www.owasp.org/index.php/REST_Security_Cheat_Sheet">OWASP REST Security Cheat Sheet</a></li>
+   <li><a href="http://www.itsecgames.com/">bWAPP (vulnerable application)</a></li>
+   <li><a href="https://sourceforge.net/projects/mutillidae/">OWASP Mutillidae II (vulnerable application)</a></li>
+  <li><a href="https://github.com/s4n7h0/xvwa">XVWA (vulnerable application)</a></li>
+    <li><a href="https://github.com/WebGoat/WebGoat">OWASP Webgoat 7 (vulnerable application)</a></li>
+</ul>
+ -->
+  
